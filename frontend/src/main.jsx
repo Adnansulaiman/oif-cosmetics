68dd7acb56f057c878351a7c89eaccf75f241834
Adnan <adnanxyz06@gmail.com>
2025-01-27 22:38:30 +0530

    Add missing UserContext.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ProductProvider } from "./context/ProductContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { UserProvider } from "./context/userContext.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
+// import { UserProvider } from "./context/UserContext.jsx";
+UserProvider
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
